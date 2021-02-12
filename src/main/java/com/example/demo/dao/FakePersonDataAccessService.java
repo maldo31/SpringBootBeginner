692428beb0fe33d4373f6893639d04a8b64689cd
203410 Malicki Daniel <203410@edu.p.lodz.pl>
2021-02-12 18:12:19 +0100

    Some work on tomcat server started

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-@Repository
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
@@ -16,6 +17,28 @@ public class FakePersonDataAccessService implements PersonDao {
     public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id, person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonbyId(UUID id, Person person) {
+        return 0;
     }
 
 }
